37ca1d5264dc234fdc14082099310b0c6893a3b1
關智瑞 <harry4558719@gmail.com>
2020-01-13 14:10:52 +0800

    Create GridEx2

---
+++ LayoutExample/LayoutExample/GridEx2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LayoutExample
+{
+    public partial class GridEx2 : ContentPage
+    {
+        public GridEx2()
+        {
+            InitializeComponent();
+        }
+    }
+}
