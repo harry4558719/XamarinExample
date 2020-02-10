de0f2cca3679ea7eb224080ea869389ef371a87f
關智瑞 <harry4558719@gmail.com>
2020-02-10 15:00:46 +0800

    新增RelativeLayout 範例

---
+++ LayoutExample/LayoutExample/RelativeLayoutEx2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LayoutExample
+{
+    public partial class RelativeLayoutEx2 : ContentPage
+    {
+        public RelativeLayoutEx2()
+        {
+            InitializeComponent();
+        }
+    }
+}
